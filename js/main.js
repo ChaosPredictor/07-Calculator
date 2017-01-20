8320c2fc7ce26ce0ea02f9a22245f504209303a9
Chaos Predictor <kuzi81@gmail.com>
2017-01-21 01:23:29 +0200

    working testing button

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 
 
-	document.getElementById("refresh").addEventListener("click", function(){
+	$( ".num-button" ).click(function() {
+		alert( "Handler for .click() called." );
+	});
 
+	document.getElementById("refresh").addEventListener("click", function(){
 	});
 
 });
